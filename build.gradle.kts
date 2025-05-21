08004244406ef95b57eaaf5f14572f4a1ec6cc70
PabloFdez06 <ffpablo@iesrafaelalberti.es.>
2025-05-21 16:36:30 +0200

    Proyecto kotlin añadido

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+    id("io.gitlab.arturbosch.detekt") version "1.23.1"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+
+    testImplementation(kotlin("test"))
+    implementation("at.favre.lib:bcrypt:0.9.0")
+    implementation("org.jline:jline:3.29.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(20)
+}
